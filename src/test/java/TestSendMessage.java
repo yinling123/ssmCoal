1a93e0c127175524b62d0680055cff5896d04018
yinling123 <2048009927@qq.com>
2022-10-14 08:41:48 +0800

    修改测试

--- src/test/java/TestSendMessage.java
+++ src/test/java/TestSendMessage.java
@@ -6,7 +6,7 @@ public class TestSendMessage {
     @Test
     public void testSend(){
         //使用工具类发送短信，返回封装的对象
-        MsgResult msgResult = SendMessage.sendMsgPost("19880036651","19880036651","58a04cd1","14AF275E","15389270371","22222");
+        MsgResult msgResult = SendMessage.sendMsgPost("19880036651","19880036651","58a04cd1","14AF275E","15389270371","3322");
         //进行判断
         if("success".equals(msgResult.getReturnstatus()) && msgResult.getCode()==0){
             System.out.println("发送成功");
